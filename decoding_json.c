6d80e8f487e17f6d08898603136abc40b5a108ee
Gregor Rayman <gregor.rayman@cloudfarms.com>
2024-05-04 06:57:22 +0200

    2024-05-04

--- decoding_json.c
+++ decoding_json.c
@@ -28,7 +28,27 @@ typedef struct _DecodingJsonData DecodingJsonData;
 struct _DecodingJsonData {
   MemoryContext context;
   bool xact_wrote_changes;
+
 };
+
+typedef struct FilterTable
+{
+  bool  add;
+	char	*schema_prefix;
+  int   schema_prefix_len;
+	char	*schema_suffix;
+  int   schema_suffic_len;
+  char  *schema_name;
+  bool  all_schemas;
+
+  char	*table_prefix;
+  int   table_prefix_len;
+	char	*table_suffix;
+  int   table_suffic_len;
+  char  *table_name;
+  bool  all_tables;
+
+} FilterTable;
 
 static void pg_decode_startup(LogicalDecodingContext* ctx, OutputPluginOptions* opt, bool is_init);
 static void pg_decode_shutdown(LogicalDecodingContext* ctx);
@@ -36,6 +56,7 @@ static void pg_decode_begin_txn(LogicalDecodingContext* ctx, ReorderBufferTXN* t
 static void pg_output_begin(LogicalDecodingContext* ctx, DecodingJsonData* data, ReorderBufferTXN* txn, bool last_write);
 static void pg_decode_commit_txn(LogicalDecodingContext* ctx, ReorderBufferTXN* txn, XLogRecPtr commit_lsn);
 static void pg_decode_change(LogicalDecodingContext* ctx, ReorderBufferTXN* txn, Relation rel, ReorderBufferChange* change);
+static void pg_decode_truncate(LogicalDecodingContext* ctx, ReorderBufferTXN* txn, int nrelations, Relation relations[], ReorderBufferChange* change);
 
 void _PG_init(void) {
 }
@@ -46,6 +67,7 @@ void _PG_output_plugin_init(OutputPluginCallbacks *cb) {
   cb->startup_cb = pg_decode_startup;
   cb->begin_cb = pg_decode_begin_txn;
   cb->change_cb = pg_decode_change;
+  cb->truncate_cb = pg_decode_truncate;
   cb->commit_cb = pg_decode_commit_txn;
   cb->shutdown_cb = pg_decode_shutdown;
 }
@@ -83,22 +105,19 @@ static void pg_output_begin(LogicalDecodingContext* ctx, DecodingJsonData* data,
     ctx->out,
     "{\"type\":\"transaction.begin\",\"xid\":\"%u\",\"committed\":\"%s\"}",
     txn->xid,
-    timestamptz_to_str(txn->commit_time)
+    timestamptz_to_str(txn->xact_time.commit_time)
   );
   OutputPluginWrite(ctx, last_write);
 }
 
 static void pg_decode_commit_txn(LogicalDecodingContext* ctx, ReorderBufferTXN* txn, XLogRecPtr commit_lsn) {
   OutputPluginPrepareWrite(ctx, true);
-  char *lsn_str = DatumGetCString(DirectFunctionCall1(pg_lsn_out, txn->end_lsn));
   appendStringInfo(
     ctx->out,
-    "{\"type\":\"transaction.commit\",\"xid\":\"%u\",\"committed\":\"%s\",\"lsn\":\"%s\"}",
+    "{\"type\":\"transaction.commit\",\"xid\":\"%u\",\"committed\":\"%s\"}",
     txn->xid,
-    timestamptz_to_str(txn->commit_time),
-    lsn_str
+    timestamptz_to_str(txn->xact_time.commit_time)
   );
-  pfree(lsn_str);
   OutputPluginWrite(ctx, true);
 }
 
@@ -164,7 +183,7 @@ static void print_value(StringInfo s, TupleDesc tupdesc, HeapTuple tuple, int i)
   bool typisvarlena;
   bool isnull;
   Oid typoutput;
-  Form_pg_attribute attr = tupdesc->attrs[i];
+  Form_pg_attribute attr = &tupdesc->attrs[i];
   Datum origval = fastgetattr(tuple, i + 1, tupdesc, &isnull);
   Oid typid = attr->atttypid;
   getTypeOutputInfo(typid, &typoutput, &typisvarlena);
@@ -182,13 +201,12 @@ static void print_value(StringInfo s, TupleDesc tupdesc, HeapTuple tuple, int i)
 
 static void tuple_to_stringinfo(StringInfo s, TupleDesc tupdesc, HeapTuple tuple, bool skip_nulls) {
   int i;
-  bool skip_comma = false;
+  bool skip_comma = true;
 
   for (i = 0; i < tupdesc->natts; i++) {
-    Form_pg_attribute attr = tupdesc->attrs[i];
+    Form_pg_attribute attr = &tupdesc->attrs[i];
 
     if (attr->attisdropped || attr->attnum < 0) {
-      skip_comma = true;
       continue;
     }
 
@@ -202,6 +220,16 @@ static void tuple_to_stringinfo(StringInfo s, TupleDesc tupdesc, HeapTuple tuple
   }
 }
 
+static 
+int string_ends_with(char * str, char * suffix) {
+  int str_len = strlen(str);
+  int suffix_len = strlen(suffix);
+
+  return 
+    (str_len >= suffix_len) &&
+    (0 == strcmp(str + (str_len-suffix_len), suffix));
+}
+
 static void pg_decode_change(LogicalDecodingContext* ctx, ReorderBufferTXN* txn, Relation relation, ReorderBufferChange* change) {
   DecodingJsonData* data;
   Form_pg_class class_form;
@@ -209,6 +237,7 @@ static void pg_decode_change(LogicalDecodingContext* ctx, ReorderBufferTXN* txn,
   HeapTuple tuple;
   MemoryContext old;
   char* table_name;
+  char* schema_name;
 
   data = ctx->output_plugin_private;
 
@@ -218,11 +247,31 @@ static void pg_decode_change(LogicalDecodingContext* ctx, ReorderBufferTXN* txn,
   tupdesc = RelationGetDescr(relation);
 
   table_name = NameStr(class_form->relname);
+  schema_name = get_namespace_name(
+      get_rel_namespace(
+        RelationGetRelid(relation)
+      )
+    );
   
   if (strncmp(table_name, "pg_temp_", 8) == 0) {
     /* ignore */
     return;
   }
+
+  /* [ Cloudfarms specific table filters */
+
+  if (strncmp(schema_name, "farm_", 5) != 0 && strncmp(schema_name, "root_", 5) != 0 && strcmp(schema_name, "public")) {
+    /* ignore */
+    return;
+  }
+
+  if (!strcmp(table_name, "journal_invalid") || string_ends_with(table_name, "_saved")) {
+    /* ignore */
+    return;
+  }
+
+  /* ] Cloudfarms specific table filters */
+
 
   old = MemoryContextSwitchTo(data->context);
 
@@ -231,14 +280,8 @@ static void pg_decode_change(LogicalDecodingContext* ctx, ReorderBufferTXN* txn,
   appendStringInfoString(ctx->out, "{\"type\":\"table\"");
   appendStringInfo(
     ctx->out,
-    ",\"schema\":\"%s\"",
-    get_namespace_name(
-      get_rel_namespace(
-        RelationGetRelid(relation)
-      )
-    )
-  );
-  appendStringInfo(ctx->out, ",\"name\":\"%s\"", table_name);
+    ",\"schema\":\"%s\"", schema_name); /* TODO: Properly escape the schema name */
+  appendStringInfo(ctx->out, ",\"name\":\"%s\"", table_name); /* TODO: Properly escape the table name */
   appendStringInfo(
     ctx->out,
     ",\"change\":\"%s\"",
@@ -251,7 +294,7 @@ static void pg_decode_change(LogicalDecodingContext* ctx, ReorderBufferTXN* txn,
           : "FIXME"
   );
 
-  if (change->action == REORDER_BUFFER_CHANGE_UPDATE || change->action == REORDER_BUFFER_CHANGE_DELETE) {
+  if (change->action == REORDER_BUFFER_CHANGE_UPDATE || change->action == REORDER_BUFFER_CHANGE_DELETE || change->action == REORDER_BUFFER_CHANGE_INSERT) {
     appendStringInfoString(ctx->out, ",\"key\":{");
     RelationGetIndexList(relation);
     if (OidIsValid(relation->rd_replidindex)) {
@@ -263,7 +306,7 @@ static void pg_decode_change(LogicalDecodingContext* ctx, ReorderBufferTXN* txn,
           : &change->data.tp.newtuple->tuple;
       for (i = 0; i < index->rd_index->indnatts; i++) {
         int j = index->rd_index->indkey.values[i];
-        Form_pg_attribute attr = tupdesc->attrs[j - 1];
+        Form_pg_attribute attr = &tupdesc->attrs[j - 1];
         if (i > 0) appendStringInfoChar(ctx->out, ',');
         appendStringInfo(ctx->out, "\"%s\":", NameStr(attr->attname));
         print_value(ctx->out, tupdesc, tuple, j - 1);
@@ -286,6 +329,81 @@ static void pg_decode_change(LogicalDecodingContext* ctx, ReorderBufferTXN* txn,
   MemoryContextReset(data->context);
 
   OutputPluginWrite(ctx, true);
+}
+static void pg_decode_truncate(LogicalDecodingContext* ctx, ReorderBufferTXN* txn, int nrelations, Relation relations[], ReorderBufferChange* change) {
+  for (int i = 0; i < nrelations; ++i) {
+    DecodingJsonData* data;
+    Form_pg_class class_form;
+    // TupleDesc  tupdesc;
+    //HeapTuple tuple;
+    MemoryContext old;
+    char* table_name;
+    char* schema_name;
+
+    data = ctx->output_plugin_private;
+
+    data->xact_wrote_changes = true;
+
+    class_form = RelationGetForm(relations[i]);
+    //tupdesc = RelationGetDescr(relations[i]);
+
+    table_name = NameStr(class_form->relname);
+    schema_name = get_namespace_name(
+        get_rel_namespace(
+          RelationGetRelid(relations[i])
+        )
+      );
+    
+    if (strncmp(table_name, "pg_temp_", 8) == 0) {
+      /* ignore */
+      return;
+    }
+
+    /* [ Cloudfarms specific table filters */
+
+    if (strncmp(schema_name, "farm_", 5) != 0 && strncmp(schema_name, "root_", 5) != 0 && strcmp(schema_name, "public")) {
+      /* ignore */
+      return;
+    }
+
+    if (!strcmp(table_name, "journal_invalid") || 
+        !strcmp(table_name, "journal") ||
+        !strcmp(table_name, "logins") || 
+        !strcmp(table_name, "farmchore") ||
+        !strcmp(table_name, "appchore") ||
+        !strcmp(table_name, "mobiledevice") ||
+        !strcmp(table_name,"danavl_update_logger") ||
+        string_ends_with(table_name, "_bak") ||         
+        string_ends_with(table_name, "_saved")) {
+      /* ignore */
+      return;
+    }
+
+    /* ] Cloudfarms specific table filters */
+
+
+    old = MemoryContextSwitchTo(data->context);
+
+    OutputPluginPrepareWrite(ctx, true);
+
+    appendStringInfoString(ctx->out, "{\"type\":\"table\"");
+    appendStringInfo(
+      ctx->out,
+      ",\"schema\":\"%s\"", schema_name); /* TODO: Properly escape the schema name */
+    appendStringInfo(ctx->out, ",\"name\":\"%s\"", table_name); /* TODO: Properly escape the table name */
+    appendStringInfo(
+      ctx->out,
+      ",\"change\":\"%s\"",
+      "TRUNCATE"
+    );
+
+    appendStringInfoChar(ctx->out, '}');
+
+    MemoryContextSwitchTo(old);
+    MemoryContextReset(data->context);
+
+    OutputPluginWrite(ctx, true);
+  }
 }
 
 /* adapted from test_decoding.c */
